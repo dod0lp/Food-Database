2579af95974e319732bbe98188edd74e39d0fb5d
dod0lp <dodotv007@gmail.com>
2024-07-13 22:24:53 +0200

    Added struct for Food.Energy

--- src/C#/Program.cs
+++ src/C#/Program.cs
@@ -1,5 +1,11 @@
-﻿namespace Food
+﻿using Food;
+
+namespace Food
 {
+    // There is no check if some values are negative, for example negative amount of Protein, Fat...
+    // If there is value not set, the default value in most, if not all, cases will be 0
+    // for easier setting up of values, so we can't say when value was not set, but we can set
+    // value of something to -1 to explicitly specify, that this value is not set yet
     public class Food
     {
         public int Id { get; set; }
@@ -75,12 +81,59 @@
         }
     }
 
+    public struct Energy
+    {
+        private static readonly double KcalToKjFactor = 4.184;
+
+        private int kcal;
+        private int kj;
+
+        public double Kcal
+        {
+            get => kcal;
+            set
+            {
+                kcal = Convert.ToInt32(value);
+                kj = Convert.ToInt32(value * KcalToKjFactor);
+            }
+        }
+
+        public double Kj
+        {
+            get => kj;
+            set
+            {
+                kj = Convert.ToInt32(value);
+                kcal = Convert.ToInt32(value / KcalToKjFactor);
+            }
+        }
+
+        public Energy(double value, bool isKcal = true)
+        {
+            if (isKcal)
+            {
+                Kcal = value;
+            }
+
+            else
+            {
+                Kj = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{kcal} kcal ({kj} kJ)";
+        }
+    }
+
+
     public struct Fat
     {
         public double Total { get; set; }
         public double Saturated { get; set; }
 
-        public Fat(double total, double saturated)
+        public Fat(double total = 0, double saturated = 0)
         {
             Total = total;
             Saturated = saturated;
@@ -102,7 +155,7 @@
         public double Total { get; set; }
         public double Sugar { get; set; }
 
-        public Carbohydrates(double total, double sugar)
+        public Carbohydrates(double total = 0, double sugar = 0)
         {
             Total = total;
             Sugar = sugar;
@@ -125,5 +178,12 @@ public class Program
     public static void Main()
     {
         Console.WriteLine("Hello World!");
+
+        Fat fat1 = new(0, 240);
+        Console.WriteLine(fat1.Total);
+        Console.WriteLine(fat1.Saturated);
+
+        Energy en1 = new(100);
+        Console.WriteLine(en1);
     }
 }

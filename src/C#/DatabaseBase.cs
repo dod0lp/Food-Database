9aa5e7d14f6cbbdbf8c5c89499474eec8cb445ec
dod0lp <dodotv007@gmail.com>
2024-07-18 01:00:53 +0200

    Added better rounding up of numbers with Tests. Added doc strings for RoundUp2Numbers Minor changes for compatibility of project.

--- src/C#/DatabaseBase.cs
+++ src/C#/DatabaseBase.cs
@@ -5,16 +5,21 @@
         private static string _server = "localhost";
         private static string _user = "user";
         private static string _password = "pw";
-        
+
         public static string connectionString = $"Server={_server};Database=db_food;User={_user};Password={_password};";
+
+        // Needs to be const because [Descriptor] this thing is used for needs const value
         public const int _max_food_description_length = 10_000;
     }
 
-    class Program
+    // TODO: Make descriptor for this very database using Food table name, class description etc.
+    // Or maybe just use LinqToSql to my predefined Food class with structs etc
+
+    /*class Program_Database
     {
         static void Main(string[] args)
         {
             Console.WriteLine();
         }
-    }
+    }*/
 }
--- src/C#/Food.cs
+++ src/C#/Food.cs
@@ -7,8 +7,35 @@
 
     public static class NumberOperations
     {
+        public static int[] lookup_powers_10 = new int[9];
+
+        static NumberOperations()
+        {
+            for (int i = 0; i < lookup_powers_10.Length; i++)
+            {
+                lookup_powers_10[i] = (int)Math.Pow(10, i);
+            }
+        }
+
+        /// <summary>
+        /// Round up double to <see cref="N"/> decimal places.<br></br>
+        /// </summary>
+        /// <param name="number">Number to be rounded up.</param>
+        /// <param name="N">Number of total decimal places.</param>
+        /// <returns></returns>
+        public static double RoundUpToNDecimalPlaces(double number, int N)
+        {
+            return Math.Ceiling(number * lookup_powers_10[N]) / lookup_powers_10[N];
+        }
+
+        /// <summary>
+        /// Specialized class for rounding up to 2decimal places.
+        /// </summary>
+        /// <param name="number">Number to be rounded up.</param>
+        /// <returns></returns>
         public static double RoundUpTo2DecimalPlaces(double number)
         {
+            // return RoundUpToNDecimalPlaces(number, 2);
             return Math.Ceiling(number * 100) / 100;
         }
     }
@@ -606,7 +633,7 @@
     }
 
 
-    public class Program
+    public class Program_Food
     {
         public static void Main()
         {
@@ -655,6 +682,11 @@
                 Console.WriteLine(fat2);
                 Carbohydrates carbs1 = new(1.1321321, 325.43891);
                 Console.WriteLine(carbs1);
+
+                for (int i = 0; i < 9; i++)
+                {
+                    Console.WriteLine(NumberOperations.RoundUpToNDecimalPlaces(10.123456789, i));
+                }
             }
         }
     }

fbcf80bb8d419dbcdcaed1dc461c44f3718f4e8f
dod0lp <dodotv007@gmail.com>
2024-07-17 22:24:42 +0200

    Added string for connecting into database ADO.NET

--- src/C#/DatabaseBase.cs
+++ src/C#/DatabaseBase.cs
@@ -2,6 +2,15 @@
 {
     public static class DB_Descriptors
     {
+        public static string connectionString = "Server=localhost;Database=db_food;User=user;Password=pw;";
         public const int _max_food_description_length = 10_000;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
     }
 }

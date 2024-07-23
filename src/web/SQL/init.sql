c2f695e545a2d4e1269ef90f27ea8a8d9da13544
dod0lp <dodotv007@gmail.com>
2024-07-23 03:10:09 +0200

    Dockerfile init creation of database works.

--- src/web/SQL/init.sql
+++ src/web/SQL/init.sql
@@ -1,31 +1,62 @@
-USE $(DB_NAME);
+USE [master];
+GO;
+
+DECLARE @DB_NAME NVARCHAR(128) = '$(DB_NAME)';
+DECLARE @DB_USER NVARCHAR(128) = '$(DB_USER)';
+DECLARE @DB_PASSWORD NVARCHAR(128) = '$(DB_PASSWORD)';
+
+CREATE TABLE Config (
+    ConfigKey NVARCHAR(128) PRIMARY KEY,
+    ConfigValue NVARCHAR(128)
+);
+
+INSERT INTO Config (ConfigKey, ConfigValue) VALUES ('DB_NAME', @DB_NAME);
+INSERT INTO Config (ConfigKey, ConfigValue) VALUES ('DB_USER', @DB_USER);
+INSERT INTO Config (ConfigKey, ConfigValue) VALUES ('DB_PASSWORD', @DB_PASSWORD);
 GO
 
--- Create user that will work with database - backend
+-- Create the database if it does not exist
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = N'$(DB_NAME)')
+BEGIN
+    EXEC sp_executesql N'CREATE DATABASE [$(DB_NAME)]';
+END
+GO
+
+-- Use the database
+USE [$(DB_NAME)];
+GO
+
+-- Create a new login (user) if it does not exist
 IF NOT EXISTS (SELECT * FROM sys.server_principals WHERE name = N'$(DB_USER)')
 BEGIN
-    EXEC sp_executesql N'CREATE LOGIN [$(DB_USER)] WITH PASSWORD = ''$(DB_PASSWORD)''';
+    CREATE LOGIN [$(DB_USER)] WITH PASSWORD = '$(DB_PASSWORD)';
 END
 GO
 
+-- Create a user in the database for Backend work
 IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = N'$(DB_USER)')
 BEGIN
-    EXEC sp_executesql N'CREATE USER [$(DB_USER)] FOR LOGIN [$(DB_USER)]';
+    CREATE USER [$(DB_USER)] FOR LOGIN [$(DB_USER)];
 END
 GO
 
+-- Grant the user necessary permissions... just simply add *all* permissions
 ALTER ROLE db_owner ADD MEMBER [$(DB_USER)];
 GO
 
 -- Create the table if it does not already exist
-IF OBJECT_ID('dbo.test_table', 'U') IS NULL
+IF OBJECT_ID('dbo.TestTable', 'U') IS NULL
 BEGIN
-    CREATE TABLE test_table (
+    CREATE TABLE TestTable (
         Id INT PRIMARY KEY IDENTITY,
         Name NVARCHAR(50) NOT NULL
     );
 END
 GO
 
-INSERT INTO test_table (Name) VALUES ('Sample Data');
+-- Insert sample data if it does not already exist
+IF NOT EXISTS (SELECT * FROM TestTable WHERE Name = 'Sample Data')
+BEGIN
+    INSERT INTO TestTable (Name) VALUES ('Sample Data');
+END
 GO

a88766a5fcb7611a6d252af42ea888850aef142e
dod0lp <dodotv007@gmail.com>
2024-07-25 09:07:25 +0200

    Added line breaks so that there are no long lines.

--- src/web/SQL/startup.sh
+++ src/web/SQL/startup.sh
@@ -4,13 +4,19 @@
 
 echo "[-----------] Waiting for SQL Server to start... [-----------]"
 
-until /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P ${SA_PASSWORD} -Q "SELECT 1" &>/dev/null
+until /opt/mssql-tools/bin/sqlcmd -S localhost \
+-U sa -P ${SA_PASSWORD} \
+-Q "SELECT 1" &>/dev/null
 do
   echo "[-----------] SQL Server is not yet available. Waiting... [-----------]"
   sleep 5s
 done
 
 echo "[-----------] Running initialization script and starting up database. [-----------]"
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P ${SA_PASSWORD} -i ${WORK_DIR}/init.sql -v WORK_DIR="${WORK_DIR}" DB_NAME=${DB_NAME} DB_USER="${DB_USER}" DB_PASSWORD="${DB_PASSWORD}"
+/opt/mssql-tools/bin/sqlcmd -S localhost \
+-U sa -P ${SA_PASSWORD} \
+-i ${WORK_DIR}/init.sql \
+-v WORK_DIR="${WORK_DIR}" DB_NAME=${DB_NAME} \
+DB_USER="${DB_USER}" DB_PASSWORD="${DB_PASSWORD}"
 
 wait

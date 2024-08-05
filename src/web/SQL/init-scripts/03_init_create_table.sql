2c45d77e51ac1a3ac82609943cd1f6c320f348b2
dod0lp <dodotv007@gmail.com>
2024-08-05 23:38:44 +0200

    Changed initialization of tables, if table named already exists - drop it and create new one with correct columns

--- src/web/SQL/init-scripts/03_init_create_table.sql
+++ src/web/SQL/init-scripts/03_init_create_table.sql
@@ -1,44 +1,47 @@
 USE [$(DB_NAME)];
 GO
 
-IF OBJECT_ID('dbo.Food', 'U') IS NULL
-BEGIN
-    CREATE TABLE dbo.Food (
-        ID INT PRIMARY KEY IDENTITY(1, 1),
-        Name NVARCHAR(100) NOT NULL,
-        Weight FLOAT NOT NULL,
-        Description NVARCHAR(4000)
-    );
-END
+IF OBJECT_ID('dbo.Food', 'U') IS NOT NULL
+    DROP TABLE dbo.Food;
 GO
 
-IF OBJECT_ID('dbo.Nutrients', 'U') IS NULL
-BEGIN
-    CREATE TABLE dbo.Nutrients (
-        ID INT PRIMARY KEY IDENTITY(1, 1),
-        FoodID INT,
-        Energy_Kcal INT NOT NULL,
-        Energy_Kj INT NOT NULL,
-        Fat_Total FLOAT NOT NULL,
-        Fat_Saturated FLOAT NOT NULL,
-        Carbs_Total FLOAT NOT NULL,
-        Carbs_Saturated FLOAT NOT NULL,
-        Protein_Total FLOAT NOT NULL,
-        Salt_Total FLOAT NOT NULL,
-        CONSTRAINT FK_Food_Nutrients FOREIGN KEY (FoodID) REFERENCES dbo.Food(food_id)
-    );
-END
+IF OBJECT_ID('dbo.Nutrients', 'U') IS NOT NULL
+    DROP TABLE dbo.Nutrients;
 GO
 
-IF OBJECT_ID('dbo.Ingredients', 'U') IS NULL
-BEGIN
-    CREATE TABLE dbo.Ingredients (
-        food_id INT NOT NULL,
-        nutrient_id INT NOT NULL,
-        quantity DECIMAL(10, 2),
-        PRIMARY KEY (food_id, nutrient_id),
-        FOREIGN KEY (food_id) REFERENCES dbo.Food(food_id),
-        FOREIGN KEY (nutrient_id) REFERENCES dbo.Nutrients(nutrient_id)
-    );
-END
+IF OBJECT_ID('dbo.Ingredients', 'U') IS NOT NULL
+    DROP TABLE dbo.Ingredients;
+GO
+
+CREATE TABLE dbo.Food (
+    ID INT PRIMARY KEY IDENTITY(1, 1),
+    Name NVARCHAR(100) NOT NULL,
+    Weight FLOAT NOT NULL,
+    Description NVARCHAR(4000)
+);
+GO
+
+CREATE TABLE dbo.Nutrients (
+    ID INT PRIMARY KEY IDENTITY(1, 1),
+    FoodID INT,
+    Energy_Kcal INT NOT NULL,
+    Energy_Kj INT NOT NULL,
+    Fat_Total FLOAT NOT NULL,
+    Fat_Saturated FLOAT NOT NULL,
+    Carbs_Total FLOAT NOT NULL,
+    Carbs_Saturated FLOAT NOT NULL,
+    Protein_Total FLOAT NOT NULL,
+    Salt_Total FLOAT NOT NULL,
+    CONSTRAINT FK_Food_Nutrients FOREIGN KEY (FoodID) REFERENCES dbo.Food(food_id)
+);
+GO
+
+CREATE TABLE dbo.Ingredients (
+    food_id INT NOT NULL,
+    nutrient_id INT NOT NULL,
+    quantity DECIMAL(10, 2),
+    PRIMARY KEY (food_id, nutrient_id),
+    FOREIGN KEY (food_id) REFERENCES dbo.Food(food_id),
+    FOREIGN KEY (nutrient_id) REFERENCES dbo.Nutrients(nutrient_id)
+);
 GO

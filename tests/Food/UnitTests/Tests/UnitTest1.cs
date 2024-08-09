8eabe7c3b3e309347e1959faf87924c889ec5adf
dod0lp <dodotv007@gmail.com>
2024-08-09 22:49:13 +0200

    Automatization for creating test values in tests

--- tests/Food/UnitTests/Tests/UnitTest1.cs
+++ tests/Food/UnitTests/Tests/UnitTest1.cs
@@ -4,10 +4,37 @@ namespace FoodTests
 {
     public class NutrientsCreation
     {
+        public static IEnumerable<object[]> TestData_Total_MultiplesOf5()
+        {
+            const int maxNum = 100;
+            const int step = 5;
+
+            for (int i = 0; i <= maxNum; i += step)
+            {
+                yield return new object[] { i };
+            }
+        }
+
+        public static IEnumerable<object[]> TestData_Total_and_Other_MultiplesOf5()
+        {
+            const int maxNum = 100;
+            const int step = 5;
+
+            Random random = new Random();
+            for (int i = 0; i <= maxNum; i += step)
+            {
+                double multiplier = random.NextDouble(); // Generates a random number between 0.0 and 0.99
+                int secondary = (int)(i * multiplier); // Multiply the Total by this secondary number (for example Saturated fat, Sugars...)
+                yield return new object[] { i, secondary };
+            }
+        }
+
+
         [Theory]
         [InlineData(10, 7)]
         [InlineData(15, 5)]
         [InlineData(20, 10)]
+        [MemberData(nameof(TestData_Total_and_Other_MultiplesOf5))]
         public void MakeFats(int totalFat, int saturatedFat)
         {
             Fat fat = new(totalFat, saturatedFat);
@@ -17,9 +44,23 @@ namespace FoodTests
         }
 
         [Theory]
+        [InlineData(30, 10)]
+        [InlineData(45, 15)]
+        [InlineData(60, 20)]
+        [MemberData(nameof(TestData_Total_and_Other_MultiplesOf5))]
+        public void MakeCarbohydrates(int total, int sugar)
+        {
+            Carbohydrates carbs = new(total, sugar);
+
+            Assert.Equal(total, carbs.Total);
+            Assert.Equal(sugar, carbs.Sugar);
+        }
+
+        [Theory]
         [InlineData(5)]
         [InlineData(10)]
         [InlineData(15)]
+        [MemberData(nameof(TestData_Total_MultiplesOf5))]
         public void MakeSalts(int total)
         {
             Salt salt = new(total);
@@ -31,6 +72,7 @@ namespace FoodTests
         [InlineData(30)]
         [InlineData(45)]
         [InlineData(60)]
+        [MemberData(nameof(TestData_Total_MultiplesOf5))]
         public void MakeProteins(int total)
         {
             Protein protein = new(total);
@@ -38,16 +80,5 @@ namespace FoodTests
             Assert.Equal(total, protein.Total);
         }
 
-        [Theory]
-        [InlineData(30, 10)]
-        [InlineData(45, 15)]
-        [InlineData(60, 20)]
-        public void MakeCarbohydrates(int total, int sugar)
-        {
-            Carbohydrates carbs = new(total, sugar);
-
-            Assert.Equal(total, carbs.Total);
-            Assert.Equal(sugar, carbs.Sugar);
-        }
     }
 }

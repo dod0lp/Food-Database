dce81f344488d5133b7a48c694e8122cfef9c2dc
dod0lp <dodotv007@gmail.com>
2024-10-30 09:58:21 +0100

    Added options to configure IP address and port in .env

--- src/web/SQL/startup.sh
+++ src/web/SQL/startup.sh
@@ -4,19 +4,21 @@
 
 echo "[-----------] Waiting for SQL Server to start... [-----------]"
 
-until /opt/mssql-tools/bin/sqlcmd -S localhost \
+until /opt/mssql-tools18/bin/sqlcmd -S ${DATABASE_ADDRESS},1433 \
 -U sa -P ${SA_PASSWORD} \
--Q "SELECT 1" &>/dev/null
+-Q "SELECT 1" &>/dev/null \
+-C
 do
   echo "[-----------] SQL Server is not yet available. Waiting... [-----------]"
   sleep 5s
 done
 
 echo "[-----------] Running initialization script and starting up database. [-----------]"
-/opt/mssql-tools/bin/sqlcmd -S localhost \
+/opt/mssql-tools18/bin/sqlcmd -S ${DATABASE_ADDRESS},1433 \
 -U sa -P ${SA_PASSWORD} \
 -i ${WORK_DIR}/init.sql \
 -v WORK_DIR="${WORK_DIR}" DB_NAME="${DB_NAME}" \
-DB_USER="${DB_USER}" DB_PASSWORD="${DB_PASSWORD}"
+DB_USER="${DB_USER}" DB_PASSWORD="${DB_PASSWORD}" \
+-C
 
 wait
